a54ed12a28ceb986e71ca5b2a1cc73e0238d6a81
kieran-lockyer <kieran.lockyer.87@gmail.com>
2018-10-26 15:30:57 +1000

    changed root route to posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   devise_for :users
-  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "home#index"
+  root "posts#index"
 end
